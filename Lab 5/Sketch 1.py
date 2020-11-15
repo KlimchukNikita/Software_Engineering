9995a22bea26f61e3a2fbdb1eb20936aee23cd63
KlimchukNikita <KlimchukNik@gmail.com>
2020-11-15 20:11:13 +0300

    Sketch 1

---
+++ Lab 5/Sketch 1.py
@@ -0,0 +1,16 @@
+class Matrix:
+    def __init__(self, A):
+        self.A = A
+ 
+    def __str__(self):
+        s = ''
+        for row in self.A:
+            for cell in row:
+                s += f'{cell} '
+            s += '\n'
+        return s
+ 
+B = [[1, 4], [2, 6]]
+
+matrix = Matrix(B)
+print(matrix)
